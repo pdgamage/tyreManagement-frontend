a3a485dbf2617ef9761ad3361fc416aef0eb5d59
Pasindu Dilshan Gamage <pdilshan704@gmail.com>
2025-06-24 10:05:13 +0530

    Update redirect URI in msalConfig to use production URL

--- src/msalConfig.ts
+++ src/msalConfig.ts
@@ -3,6 +3,6 @@ export const msalConfig = {
     clientId: "bca262cc-e31e-460e-b9b2-342fac6536ff",
     authority:
       "https://login.microsoftonline.com/a92cbcff-6e56-42ae-9e7f-742431739e80",
-    redirectUri: "http://localhost:3000",
+    redirectUri: "https://tyre-management-frontend.vercel.app/",
   },
 };

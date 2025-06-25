769cfd57614d4d85dc1f5b702cccd767afb6bfe4
Pasindu Dilshan Gamage <pdilshan704@gmail.com>
2025-06-25 14:44:36 +0530

    Refactor index.tsx to use ReactDOM.createRoot for rendering and wrap MsalProvider in React.StrictMode for improved performance and structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import "./index.css";
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
 import { App } from "./App";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
-import { msalConfig } from "./msalConfig";
+import {msalConfig} from "./msalConfig"; 
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
-render(
-  <MsalProvider instance={msalInstance}>
-    <App />
-  </MsalProvider>,
-  document.getElementById("root")
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
+  </React.StrictMode>
 );

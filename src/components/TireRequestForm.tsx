68b943669aec3b45b3272915d39b3c2b159756c3
Pasindu Dilshan Gamage <pdilshan704@gmail.com>
2025-07-01 10:13:41 +0530

    modified:   src/components/TireRequestForm.tsx

--- src/components/TireRequestForm.tsx
+++ src/components/TireRequestForm.tsx
@@ -392,19 +392,19 @@ const RequestInformationStep: React.FC<StepProps> = ({
             required
           >
             <option value="">Select Department</option>
-            <option value="Field_Operations">
+            <option value="Field Operations">
               Field Operations / Service Delivery
             </option>
-            <option value="Logistics_Distribution">
+            <option value="Logistics & Distribution">
               Logistics & Distribution
             </option>
-            <option value="Sales_Marketing">Sales & Marketing</option>
-            <option value="Customer_Support">Customer Support </option>
-            <option value="Maintenance_Technical_Support">
+            <option value="Sales & Marketing">Sales & Marketing</option>
+            <option value="Customer Support">Customer Support </option>
+            <option value="Maintenance & Technical Support">
               Maintenance & Technical Support
             </option>
             <option value="Security">Security</option>
-            <option value="Training_HR">Training & HR</option>
+            <option value="Training & HR">Training & HR</option>
           </select>
           {/* <input
             type="text"

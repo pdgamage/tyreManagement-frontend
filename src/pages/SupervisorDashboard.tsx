b241e52a72111769f71c0412276d2b793eb62d5e
suresh <suresh68004@gmail.com>
2025-08-12 20:19:33 +0530

    fix: Update button label from "User Inquiry" to "Supervisor Inquiry" in SupervisorDashboard

--- src/pages/SupervisorDashboard.tsx
+++ src/pages/SupervisorDashboard.tsx
@@ -166,7 +166,7 @@ const SupervisorDashboard = () => {
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              <span>User Inquiry</span>
+              <span>Supervisor Inquiry</span>
             </button>
           </div>
         </div>
